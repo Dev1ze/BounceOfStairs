6e2429b895279e8a6050cd0c990f266b2dc57363
Артем <iron-man1998@mail.ru>
2023-09-01 22:40:34 +0300

    Добавлены перекаты и улучшен прыжок

--- Assets/Scripts/BallMoving.cs
+++ Assets/Scripts/BallMoving.cs
@@ -19,23 +19,49 @@ public class BallMoving : MonoBehaviour
     void Update()
     {
         Jumping();
+        Moving();
     }
     private void OnCollisionStay(Collision collision)
     {
-        if(collision.gameObject.tag == "Ground") isGround = true;
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
+            _rigidbody.drag = 4;
+        }
 
     }
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.tag == "Ground") isGround = false;
+        if (collision.gameObject.tag == "Ground") 
+        {
+            isGround = false;
+            _rigidbody.drag = 0;
+        }
+
     }
 
     void Jumping()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isGround) //Input.TouchCount > 0 = GetKeyDown(KeyCode.Space)
         {
+            _rigidbody.velocity = new Vector3(0, _jumpForce, 0);
             _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
             _rigidbody.AddForce(Vector3.forward * _speedForce, ForceMode.Force);
+        }
+    }
+    void Moving()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && isGround)
+        {
+
+            _rigidbody.velocity = new Vector3(-_jumpForce, 0, 0);
+            _rigidbody.AddForce(Vector3.left * _jumpForce , ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.D) && isGround)
+        {
+            _rigidbody.drag = 4;
+            _rigidbody.velocity = new Vector3(_jumpForce, 0, 0);
+            _rigidbody.AddForce(Vector3.right * _jumpForce, ForceMode.Impulse);
         }
     }
 
