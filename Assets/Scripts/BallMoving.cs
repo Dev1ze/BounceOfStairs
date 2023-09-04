f2b4e168ce367e449ff357c663641f84b4ae4562
Артем <iron-man1998@mail.ru>
2023-09-04 07:27:37 +0300

    Управление под Android

--- Assets/Scripts/BallMoving.cs
+++ Assets/Scripts/BallMoving.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class BallMoving : MonoBehaviour
 {
-    [SerializeField] private Vector3 _startPosition;
     [SerializeField] private float _jumpForce;
     [SerializeField] private float _speedForce;
     [SerializeField] private bool isGround;
@@ -13,56 +12,59 @@ public class BallMoving : MonoBehaviour
     private Rigidbody _rigidbody;
     void Start()
     {
-        transform.position = _startPosition;
         _rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        Jumping();
-        Moving();
+        if (Input.touchCount > 0)
+        {
+            ForwardJump();
+            RightOrLeftJump();
+        }
     }
     private void OnCollisionStay(Collision collision)
     {
-        if(collision.gameObject.tag == "Ground")
-        {
-            isGround = true;
-        }
-
+        if(collision.gameObject.tag == "Ground") isGround = true;
     }
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.tag == "Ground") 
-        {
-            isGround = false;
-        }
+        if (collision.gameObject.tag == "Ground") isGround = false;
     }
 
-    void Jumping()
+    void ForwardJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGround) //Input.TouchCount > 0 = GetKeyDown(KeyCode.Space)
+
+        Touch touch = Input.GetTouch(0);
+        Vector3 delta = Input.GetTouch(0).deltaPosition;
+        if (touch.phase == TouchPhase.Ended && delta.x == 0 && isGround)
         {
             _rigidbody.velocity = new Vector3(0, _jumpForce, 0);
             _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
             _rigidbody.AddForce(Vector3.forward * _speedForce, ForceMode.Force);
         }
     }
-    void Moving()
+    void RightOrLeftJump()
     {
-        if (Input.GetKeyDown(KeyCode.A) && isGround && _countSwipe < 2)
+
+        Touch touch = Input.GetTouch(0);
+        Vector3 delta = Input.GetTouch(0).deltaPosition;
+        if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
         {
-            _countSwipe++;
-            _rigidbody.velocity = new Vector3(-_jumpForce, 0, 0);
-            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-            _rigidbody.AddForce(Vector3.left * _jumpForce , ForceMode.Impulse);
-        }
-        if (Input.GetKeyDown(KeyCode.D) && isGround && _countSwipe > -2)
-        {
-            _countSwipe--;
-            _rigidbody.velocity = new Vector3(_jumpForce, 0, 0);
-            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-            _rigidbody.AddForce(Vector3.right * _jumpForce, ForceMode.Impulse);
+            if (delta.x < 0 && touch.phase == TouchPhase.Moved && isGround && _countSwipe < 2)
+            {
+                _countSwipe++;
+                _rigidbody.velocity = new Vector3(-_jumpForce, 0, 0);
+                _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+                _rigidbody.AddForce(Vector3.left * _jumpForce, ForceMode.Impulse);
+            }
+            if (delta.x > 0 && touch.phase == TouchPhase.Moved && isGround && _countSwipe > -2)
+            {
+                _countSwipe--;
+                _rigidbody.velocity = new Vector3(_jumpForce, 0, 0);
+                _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+                _rigidbody.AddForce(Vector3.right * _jumpForce, ForceMode.Impulse);
+            }
         }
     }
-
 }
