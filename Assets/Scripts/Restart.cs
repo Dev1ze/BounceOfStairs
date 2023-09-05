9eb51e901f772acfde4e580f49eb0d08339d5d1d
Артем <iron-man1998@mail.ru>
2023-09-05 21:59:04 +0300

    Воспроизведение Partical Sistems при уничтожении шарика

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 public class Restart : MonoBehaviour
 {
+    [SerializeField] GameObject Explosion;
     async private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Enemy")
         {
             Destroy(gameObject);
+            Instantiate(Explosion, transform.position, Quaternion.identity);
             await Task.Delay(2000);
             SceneManager.LoadScene(0);
-
         }
     }
 }
